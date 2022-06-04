036f324252111fb0eefbc21a67f4aa5ae556158d
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-06-04 21:40:47 +0900

    Update 괄호.java

--- 괄호.java
+++ 괄호.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class 괄호 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		int num;
+		int left = 0;
+		int right = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		for(int i = 0 ; i < num ; i++) {
+			String text = sc.nextLine();
+			
+			for(int j = 0 ; j <text.length() ; j++) {
+				if(text.substring(j) == "(");
+				{
+					left++;
+				}
+				if(text.substring(j) == ")")
+				{
+					right++;
+				}
+			}
+			
+			if(right == left)
+			{
+				System.out.println("YES");
+			}
+			else
+			{
+				System.out.println("NO");
+			}
+			right = 0;
+			left = 0;
+		}
 	}
+
+	
 
 }
